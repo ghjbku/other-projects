82d27973275e43d3350de584b2bfd861efe54071
Balázs Tóth <gawnoris@gmail.com>
2019-12-10 13:58:44 +0100

    Update cuccok.rkt

--- cuccok.rkt
+++ cuccok.rkt
@@ -65,3 +65,71 @@
 (nextlex '(1 2 3 4))
 (nextlex '(1 2 4 3))
 (nextlex '(1 3 2 4))
+
+
+#lang racket
+
+(define (fibo n acc_1 acc_2)
+  (let recFibo ((n n) (acc_1 acc_1) (acc_2 acc_2))
+    (if (< n 2) acc_1 (recFibo (- n 1) acc_2 (+ acc_1 acc_2)))))
+
+(fibo 10 0 1)
+
+(letrec ((fiboo (lambda (n acc_1 acc_2)
+                (if (< n 2) acc_1 (fiboo (- n 1) acc_2 (+ acc_1 acc_2))))))
+  (fiboo 10 0 1))
+  
+  
+  #lang racket
+
+ 
+
+; n = 23 esetén a Collatz-sorozat hossza 16
+
+
+(define (collatzif n c)
+  (if (= n 1)
+      c
+      (if (even? n)
+          (collatzif (/ n 2) (+ c 1))
+          (collatzif (+ (* 3 n) 1) (+ c 1)))))
+
+(collatzif 23 1)
+
+(define (collatzcond n c)
+  (cond
+    [(= n 1) c]
+    [(even? n) (collatzcond (/ n 2) (+ c 1))]
+    [else (collatzcond (+ (* 3 n) 1) (+ c 1))]
+    ))
+
+
+(collatzcond 23 1)
+
+(define (collatzlet n)
+  (let loop ((n n) (c 1))
+    (cond
+      [(= n 1) c]
+      [(even? n) (loop (/ n 2) (+ c 1))]
+      [else (loop (+ (* 3 n) 1) (+ c 1))]
+    )))
+
+(collatzlet 23)
+
+(define (collatzletrec n)
+  (letrec ((iter (lambda (n c)
+                   (cond
+                     [(= n 1) c]
+                     [(even? n) (iter (/ n 2) (+ c 1))]
+                     [else (iter (+ (* 3 n) 1) (+ c 1))]
+                    ))))
+    (trace iter)
+    (iter n 1)))
+
+
+(collatzletrec 23)
+
+(define (collatzdo n)
+  (do ([n1 n (if (even? n1) (/ n1 2) (+ (* 3 n1) 1))] [c 1 (+ c 1)]) ((= n1 1) c)))
+
+(collatzdo 23)
