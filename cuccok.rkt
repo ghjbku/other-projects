f145143fd00bb7289363e889b8ea6250ed3134d3
Balázs Tóth <gawnoris@gmail.com>
2019-12-10 13:56:29 +0100

    Update cuccok.rkt

--- cuccok.rkt
+++ cuccok.rkt
@@ -7,3 +7,20 @@
           ((and (not (null? bb)) [equal? (car bb) (car m)]) (helper aa (cdr bb) (cdr m)))
           (else #f)
           )))
+
+(define (zipper a b c)
+  (let ((a-lst (string->list a)) (b-lst (string->list b)) (c-lst (string->list c)))
+    (define (zipper/list a-lst b-lst c-lst)
+      (cond
+        [(null? c-lst) #t]
+        [(and (not (null? a-lst)) (not (null? b-lst)) (equal? (car a-lst) (car c-lst)) (equal? (car b-lst) (car c-lst)))
+         (or (zipper/list (cdr a-lst) b-lst (cdr c-lst)) (zipper/list a-lst (cdr b-lst) (cdr c-lst)))]
+        [(and (not (null? a-lst)) (equal? (car a-lst) (car c-lst))) (zipper/list (cdr a-lst) b-lst (cdr c-lst))]
+        [(and (not (null? b-lst)) (equal? (car b-lst) (car c-lst))) (zipper/list a-lst (cdr b-lst) (cdr c-lst))]
+        [else #f]))
+    (zipper/list a-lst b-lst c-lst)))
+
+(zipper "cat" "tree" "tcraete")
+(zipper "cat" "tree" "catrtee")
+(zipper "cat" "tree" "cttaree")
+(zipper "catcatcatcatcat" "treetreetreetreetree" "catrteecatrteecatrteecatrteecatrtee")
